da85b551c94e7c63cd86d1e3acaa31588fb37e26
Cristiano Buczak <buczak@gmail.com>
2024-08-28 20:18:23 -0300

    Alterando URL da API

--- desafio-everymind-frontend/src/services/api.js
+++ desafio-everymind-frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
-const api = axios.create({
+/* const api = axios.create({
     baseURL: 'http://localhost:3000'
+}) */
+
+const api = axios.create({
+    baseURL: 'https://desafio-everymind-nxfp.vercel.app'
 })
 
 export default api
